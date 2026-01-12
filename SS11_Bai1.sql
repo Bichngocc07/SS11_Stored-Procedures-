39b915bb554bae7affa2b2107fc5ec50050fee3c
Bichngocc07 <ngoc01072006@gmail.com>
2026-01-12 22:35:04 +0700

    Update SS11_Bai1.sql

--- SS11_Bai1.sql
+++ SS11_Bai1.sql
@@ -1,11 +1,7 @@
 -- 1. SỬ DỤNG DATABASE
--- ==============================
 USE social_network_pro;
 
--- ==============================
 -- 2. TẠO STORED PROCEDURE
--- Lấy danh sách bài viết của 1 user
--- ==============================
 DELIMITER //
 
 CREATE PROCEDURE sp_GetPostsByUser (
@@ -23,13 +19,8 @@ END //
 
 DELIMITER ;
 
--- ==============================
 -- 3. GỌI STORED PROCEDURE
--- Ví dụ: lấy bài viết của user_id = 1
--- ==============================
 CALL sp_GetPostsByUser(1);
 
--- ==============================
 -- 4. XÓA STORED PROCEDURE
--- ==============================
 DROP PROCEDURE IF EXISTS sp_GetPostsByUser;
